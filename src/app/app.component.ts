d1cae18cba9b57647fdb3c8a419bed1b253ac89f
GhInkO <gatman-00@mail.ru>
2020-04-30 18:04:54 +0300

    commitv3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,122 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html',  
   styleUrls: [ './app.component.css' ]
 })
 
-export class AppComponent {        
+export class AppComponent implements OnInit {        
 
-  counter: number = 0;  
+  ngOnInit() {
+    this.addIndics();
+  }
+  @ViewChild('idinput') idinput: ElementRef;
+  @ViewChild('nameinput') nameinput: ElementRef;
+  @ViewChild('indicinput') indicinput: ElementRef;
+  
+  checker: boolean = false;
+  counter: number = 0;    
 
   //Default 10 indics
-  addIndics() {
-    let counter: number = 0;
+  addIndics() {    
     
-    while(this.counter<10) {
+    while(this.counter<10) {      
       let indic: string = Math.random() >= 0.5 ? '#6ee755':'#f05959';
       let name: string = this.names[Math.floor(Math.random()*this.names.length)];
-      let div = document.createElement('div');   
-      div.id = this.counter.toString();   
-    
-      div.innerHTML = `      
-        <div class="card" style="
-          background-color: ` + indic + `; 
-          width: 18rem;          
-          margin: 1.5em;            
-          float: left;
-        ">
-          <div class="card-body">
-            <p class="card-text">ID: ` + this.counter + `.</p>
-            <p class="card-text">Name: ` + name + `.</p>
-            <button id="but`+ this.counter +`" type="button" class="btn btn-outline-secondary" style="
-              border: 1.5px solid black; color: black;
-            ">Удалить</button>                        
-          </div>      
-        </div>
-      `;         
-      document.getElementById('maind').appendChild(div);         
-
-      document.getElementById('but'+this.counter).addEventListener('click', () => {
-        this.deleteIndic(div.id);
-      });                                  
-
-      this.idholder.push(this.counter.toString());      
-      this.counter++;      
+      this.cardsholder.push({ id: this.counter.toString(), color: indic, name: name }); 
+      console.log(this.cardsholder);         
+      this.counter++;                   
     } 
   }
 
   //Button action
   addIndic() {       
-
-    let checker = true;
-    let indic: string = (<HTMLInputElement>document.getElementById('exampleCheck1')).checked ? '#6ee755':'#f05959';            
-    let name = (<HTMLInputElement>document.getElementById("exampleInputName1")).value;  
-    let id: string = (<HTMLInputElement>document.getElementById("exampleInputID")).value;        
+        
+    let indic: string = this.indicinput.nativeElement.checked ? '#6ee755':'#f05959';
+    let name: string = this.nameinput.nativeElement.value;
+    let id: string = this.idinput.nativeElement.value;                  
       
-    for(let i=0; i<this.idholder.length; i++) {
-      if(id != this.idholder[i]) {
+    for(let i=0; i<this.cardsholder.length; i++) {
+      if(id != this.cardsholder[i].id) {
         continue
       }
       else {
-        (<HTMLInputElement>document.getElementById("exampleInputID")).value = '';
-        return (<HTMLInputElement>document.getElementById("exampleInputID")).placeholder = 'Такой ID уже имеется.' 
+        this.idinput.nativeElement.value = '';     
+        return this.idinput.nativeElement.placeholder = 'Этот ID уже занят'
       }
-    }       
-    if(!(+id)) {  
-      (<HTMLInputElement>document.getElementById("exampleInputID")).value = '';    
-      return (<HTMLInputElement>document.getElementById("exampleInputID")).placeholder = 'Задайте ID.'
+    }
+    if(!id.match(/^[0-9]+$/)) {
+      this.idinput.nativeElement.value = '';
+      return this.idinput.nativeElement.placeholder = 'Некорректный ID'
     }
     if(!name.match(/^[a-zA-Z0-9а-яА-Я]+$/)) {
-      (<HTMLInputElement>document.getElementById("exampleInputName1")).value = '';
-      return (<HTMLInputElement>document.getElementById("exampleInputName1")).placeholder = 'Задайте имя.'
-    }
+      this.nameinput.nativeElement.value = '';
+      return this.nameinput.nativeElement.placeholder = 'Некорректное имя'
+    }     
 
-    let div = document.createElement('div');     
-            
-    div.id = id.toString();
-    div.innerHTML = `      
-      <div class="card" style="
-      background-color: ` + indic + `; 
-      width: 18rem;          
-      margin: 1.5em;            
-      float: left;
-      ">
-        <div class="card-body">
-          <p class="card-text">ID: ` + id + `.</p>
-          <p class="card-text">Name: ` + name + `.</p>
-          <button id="but`+ id +`" type="button" class="btn btn-outline-secondary" style="
-            border: 1.5px solid black; color: black;
-          ">Удалить</button>
-        </div>      
-      </div>        
-    `;          
-    
-    document.getElementById('maind').appendChild(div);
-
-    document.getElementById('but'+id).addEventListener('click', () => {
-      this.deleteIndic(div.id);
-    });
-    this.idholder.push(id);
-               
+    this.cardsholder.push({ id: id, color: indic, name: name });    
+    this.checker = true;
+    this.checker = false;    
   }
 
   //Delete indicator
-  deleteIndic(delid: string): any {
-    this.idholder.forEach((item) => {
-      if(delid === item) {
-        const i = this.idholder.indexOf(item);
-        this.idholder.splice(i, 1);
+  deleteIndic(delid: string) {
+    this.cardsholder.forEach((item, i) => {
+      if(delid === item.id) {        
+        this.cardsholder.splice(i, 1);
       }
-    })
-    document.getElementById(delid.toString()).remove();
-    console.log(this.idholder);
+    })        
   }
 
   //Data arrays
-  names: string[] = ['Rock', 'Paper', 'Fortune', 'Drama', 'Version', 'Writer', 'Trainer', 'Chocolate', 'Drawer', 'Blood', 'Safety', 'Secretary', 'Ability', 'Bread', 'Currency', 'Temperature'];
-  idholder: string[] = [];
-
+  names: string[] = ['Rock', 'Paper', 'Fortune', 'Drama', 'Version', 'Writer', 'Trainer', 'Chocolate', 'Drawer', 'Blood', 'Safety', 'Secretary', 'Ability', 'Bread', 'Currency', 'Temperature'];  
+  cardsholder: {id: string, name: string, color: string}[] = [];
 }
 
